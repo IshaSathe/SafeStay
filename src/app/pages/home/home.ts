69e0cdd7bb394e46cd863760f1d3b5defebadc93
rgsathe <rgsathe@asu.edu>
2025-09-28 02:36:42 -0700

    home-page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, NgZone  } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +8,8 @@ import { interval } from 'rxjs';
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home{
 
-  images: string[] = [
-    'https://img1.10bestmedia.com/Images/Photos/419369/GettyImages-1404885159_54_990x660.jpg?auto=webp&width=3840&quality=75',
-    'logo1.png'
-  ];
-  currentIndex = 0;
-
-   constructor() {
-    interval(5000).subscribe(() => {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
-      console.log('Switched to:', this.images[this.currentIndex]);
-    });
-  }
 
 
 }
